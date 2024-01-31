eaac69fd0de764f64ee4efc92f235ba346389a8f
saraavr <143004455+saraavr@users.noreply.github.com>
2024-01-31 18:09:30 -0300

    Update style.css

---
+++ style.css
@@ -0,0 +1,26 @@
+canvas {
+        border: 1px solid gray;
+  background: white;
+    }
+body {
+  font-family: system-ui;
+  background: Pink;
+  color: Red;
+  text-align: center;
+}
+
+.button {
+    font-family: system-ui;
+    font-weight: bold;
+    font-size: 16px;
+    color: White;
+  background: Red;
+  border:2px solid red;
+    
+    /* Adicione outras propriedades de estilo conforme necessário */
+}
+
+#p {
+  font-family: system-ui;
+  font-weight: bold;
+}
