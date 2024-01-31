5772c27cdc8fbc7b70bbd65887e15b9a9b83356b
saraavr <143004455+saraavr@users.noreply.github.com>
2024-01-31 18:09:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,74 @@
-canvas {
-        border: 1px solid gray;
-  background: white;
-    }
-body {
-  font-family: system-ui;
-  background: Pink;
-  color: Red;
-  text-align: center;
+var paintcanvas = document.getElementById("canvas1");
+var context = paintcanvas.getContext("2d");
+var color = 'black';
+var radius = 50;
+// only paint if mouse is  being dragged (moved while the button is pressed)
+var isPainting = false;
+function startPaint(){
+  isPainting = true;
 }
 
-.button {
-    font-family: system-ui;
-    font-weight: bold;
-    font-size: 16px;
-    color: White;
-  background: Red;
-  border:2px solid red;
-    
-    /* Adicione outras propriedades de estilo conforme necessário */
+function endPaint(){
+  isPainting = false ;
 }
 
-#p {
-  font-family: system-ui;
-  font-weight: bold;
+function doPaint(x,y){
+  if(isPainting){
+    var canvas = document.getElementById("canvas1");
+  var ctx = canvas.getContext("2d");
+   paintCircle(x,y);
+  }
+}
+
+function setWidth (value) {
+ var widthInput = document.getElementById("widthInput");
+    var widthValue = parseInt(widthInput.value);
+  var canvas = document.getElementById("canvas1");
+
+        // Definir a largura do canvas
+        canvas.width = widthValue;
+
+        // Limpar o conteúdo do canvas (opcional)
+        var ctx = canvas.getContext("2d");
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function setHeight (value) {
+ var heightInput = document.getElementById("heightInput");
+    var heightValue = parseInt(heightInput.value);
+  var canvas = document.getElementById("canvas1");
+
+        // Definir a largura do canvas
+        canvas.height = heightValue;
+
+        // Limpar o conteúdo do canvas (opcional)
+        var ctx = canvas.getContext("2d");
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function setColor(newColor){
+  color=newColor
+}
+
+function Brush(newSize){
+  radius=newSize;
+  document.getElementById("sizeOutput").value=newSize
+}
+
+function clearCanvas () {
+  context.clearRect(0, 0, paintcanvas.width, paintcanvas.height);
+}
+
+function paintCircle (x, y) {
+    // make sure to start a new circle each time
+    context.beginPath();
+    // draw circle using a complete (2*PI) arc around given point
+    context.arc(x, y, radius, 0, Math.PI * 2, true);
+    context.fillStyle = color;
+    context.fill();
+}
+// verify the given value is actually a number
+function isNumeric (value) {
+  // standard JavaScript function to determine whether a string is an illegal number (Not-a-Number)
+  return !isNaN(value);
 }
